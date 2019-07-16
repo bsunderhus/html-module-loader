cf03911d14a325004a879fc4bf816ba5dc262e33
Bernardo Lucas Almeida Sunderhus <bernardo.sunderhus@gmail.com>
2019-07-16 16:06:34 -0300

    Removes dist from coverage

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,7 @@
 module.exports = {
-	roots: ['<rootDir>/src', '<rootDir>/tests', '<rootDir/dist>'],
+	roots: ['<rootDir>/src', '<rootDir>/tests'],
 	transform: {
 		'^.+\\.[t|j]sx?$': 'babel-jest'
-	}
+	},
+	collectCoverageFrom: ["src/*.ts"]
 }
